0bbb557c3ee08597ec5d38247383448ef327ff08
PointerFLY <linghao.michael.ma@gmail.com>
2018-02-17 13:51:00 +0000

    Name Baselines interface better

--- src/main/java/Baselines.java
+++ src/main/java/Baselines.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 class Baselines extends ArrayList<ArrayList<Integer>> {
 
-    ArrayList<Integer> getRelevantDocuments(int id) {
-        return this.get(id);
+    ArrayList<Integer> getRelevantDocumentIds(int queryId) {
+        return this.get(queryId);
     }
 }
