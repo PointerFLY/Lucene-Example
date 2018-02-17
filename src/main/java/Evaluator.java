5177682a41ec860847ef726127fcdb2537efa1d7
PointerFLY <linghao.michael.ma@gmail.com>
2018-02-17 13:05:35 +0000

    Evaluator basic interfaces

---
+++ src/main/java/Evaluator.java
@@ -0,0 +1,17 @@
+class Evaluator {
+
+    private Searcher searcher;
+    private BaselinesModel baselines;
+
+    Evaluator(Searcher searcher, BaselinesModel baseline) {
+        
+    }
+
+    void evaluateMAP() {
+
+    }
+
+    void evaluateRecall() {
+
+    }
+}
