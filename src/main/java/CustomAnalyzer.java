d0c18f37f4f593f2db58db8e90157aca75d3679f
PointerFLY <linghao.michael.ma@gmail.com>
2018-02-19 21:46:00 +0000

    CustomAnalyzer

--- src/main/java/CustomAnalyzer.java
+++ src/main/java/CustomAnalyzer.java
@@ -1,9 +1,17 @@
-import org.apache.lucene.analysis.StopwordAnalyzerBase;
+import org.apache.lucene.analysis.*;
+import org.apache.lucene.analysis.core.StopAnalyzer;
+import org.apache.lucene.analysis.en.EnglishMinimalStemFilter;
+import org.apache.lucene.analysis.standard.ClassicTokenizer;
 
-public class CustomAnalyzer extends StopwordAnalyzerBase {
+public class CustomAnalyzer extends Analyzer {
 
     @Override
     protected TokenStreamComponents createComponents(String fieldName) {
-        return null;
+        Tokenizer source = new ClassicTokenizer();
+        TokenStream token = new LowerCaseFilter(source);
+        CharArraySet stopSet = CharArraySet.copy(StopAnalyzer.ENGLISH_STOP_WORDS_SET);
+        token = new StopFilter(token, stopSet);
+        token = new EnglishMinimalStemFilter(token);
+        return new TokenStreamComponents(source, token);
     }
 }
