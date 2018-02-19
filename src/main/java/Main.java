b9ce8510de37318b5cd0b5a34eb8d70e89c55d34
PointerFLY <linghao.michael.ma@gmail.com>
2018-02-19 11:08:46 +0000

    EvaluateAll()

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,8 @@
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.search.similarities.BM25Similarity;
+import org.apache.lucene.search.similarities.BooleanSimilarity;
 import org.apache.lucene.search.similarities.ClassicSimilarity;
+import org.apache.lucene.search.similarities.MultiSimilarity;
 
 import java.util.ArrayList;
 
@@ -18,10 +20,10 @@ public class Main {
 
         // BM25 Model
         searcher.setSimilarity(new BM25Similarity());
-        evaluator.evaluateAll();
+        evaluator.evaluateAll(false);
 
         // Vector Space Model
         searcher.setSimilarity(new ClassicSimilarity());
-        evaluator.evaluateAll();
+        evaluator.evaluateAll(false);
     }
 }
